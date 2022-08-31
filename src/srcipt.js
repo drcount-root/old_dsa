12af44b12404b9246acc998db943d5455d9a2731
Subham Sahu <about.subhamsahu@gmail.com>
2022-08-31 22:17:04 +0530

    31th Aug 2022 - C2

--- src/srcipt.js
+++ src/srcipt.js
@@ -1,13 +1,95 @@
-const nemo = ["nemo"];
+// const nemo = ["nemo"];
 
-function findNemo(arr) {
-  let ans = "";
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "nemo") {
-      ans = "Found NEMO!";
-    }
-  }
-  return ans;
+// function findNemo(arr) {
+//   let ans = "";
+
+//   let t0 = performance.now();
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "nemo") {
+//       ans = "Found NEMO!";
+//     }
+//   }
+
+//   let t1 = performance.now();
+
+//   let res = `${ans} & Call to find Nemo took ${t1 - t0} miliseconds`;
+
+//   return res;
+// }
+
+// console.log(findNemo(nemo));
+
+// ----------------------------------------------------------------------
+
+// const nemo = ["nemo"];
+// const everyone = [
+//   "dory",
+//   "bruce",
+//   "marlin",
+//   "nemo",
+//   "gill",
+//   "bloat",
+//   "nigel",
+//   "squirt",
+//   "darla",
+//   "hank",
+// ];
+
+// const large = new Array(100).fill("nemo");
+
+// function findNemo(arr) {
+//   let t0 = performance.now();
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "nemo") {
+//       console.log("Found NEMO!");
+//     }
+//   }
+
+//   let t1 = performance.now();
+
+//   return `Call to find Nemo took ${t1 - t0} miliseconds`;
+// }
+
+// console.log(findNemo(nemo));
+// console.log(findNemo(everyone));
+// console.log(findNemo(large));
+
+// ----------------------------------------------------------------------
+
+// const nemo = ["nemo"];
+// const everyone = [
+//   "dory",
+//   "bruce",
+//   "marlin",
+//   "nemo",
+//   "gill",
+//   "bloat",
+//   "nigel",
+//   "squirt",
+//   "darla",
+//   "hank",
+// ];
+
+// const large = new Array(100).fill("nemo");
+
+// function findNemo(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "nemo") {
+//       console.log("Found NEMO!");
+//     }
+//   }
+// }
+
+// console.log(findNemo(nemo));
+// console.log(findNemo(everyone));
+// console.log(findNemo(large));
+
+// -------------------------------------------------------------
+
+function compressFirstBox(boxes) {
+  console.log(boxes[0]);
 }
 
-console.log(findNemo(nemo));
+compressFirstBox([7, 1, 4, 6]);
