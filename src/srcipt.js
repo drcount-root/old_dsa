7f08d07f2d2edeee3b5d7a8f8c85c7d87c04a957
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-01 09:42:47 +0530

    1st Sept 2022 C1

--- src/srcipt.js
+++ src/srcipt.js
@@ -88,8 +88,45 @@
 
 // -------------------------------------------------------------
 
-function compressFirstBox(boxes) {
-  console.log(boxes[0]);
+// function compressFirstBox(boxes) {
+//   console.log(boxes[0]);
+// }
+
+// compressFirstBox([7, 1, 4, 6]);
+
+// -------------------------------------------------
+// function funChallenge(input) {
+//   let a = 10;
+//   a = 50 + 3;
+
+//   for (let i = 0; i < input.length; i++) {
+//     anotherFunction();
+//     let stranger = true;
+//     a++;
+//   }
+//   return a;
+// }
+
+// -------------------------------------------------
+
+function anotherFunChallenge(input) {
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+  for (let i = 0; i < input; i++) {
+    // O(n)
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
+  }
+  for (let j = 0; j < input; j++) {
+    // O(n)
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
+  }
+  let whoAmI = "I don't know"; // O(1)
 }
 
-compressFirstBox([7, 1, 4, 6]);
+// O(4+7n) => O(n)
+
+// -------------------------------------------------
