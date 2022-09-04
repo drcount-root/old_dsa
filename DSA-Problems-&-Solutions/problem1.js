b0b7bdce73a0e07387c71dd5b010f3fcb2248132
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-04 18:44:01 +0530

    4th Sept 2022 - C1

--- DSA-Problems-&-Solutions/problem1.js
+++ DSA-Problems-&-Solutions/problem1.js
@@ -13,5 +13,42 @@
 
 // -----------------------------------------------------------------------------------
 
-// Approach - 1
+// [ Approach - 1 ]  BIG-O Time Complexity = O(a*b)
 
+// const arr1 = ["a", "b", "c", "x"];
+// const arr2 = ["z", "y", "i"];
+
+// const commonFinder = function (arr1, arr2) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// };
+
+// console.log(commonFinder(arr1, arr2));
+
+// -----------------------------------------------------------------------------------
+
+// [ Approach - 2 ]
+
+const arr1 = ["a", "b", "c", "x"];
+const arr2 = ["z", "y", "i"];
+
+// arr1 => obj{
+//     a: true,
+//     b: true,
+//     c: true,
+//     x: true
+// }
+// arr2[index] === obj.properties
+
+const containsCommonsFunc = function (arr1, arr2) {
+  // loop through first array and create object where properties === items in the array
+  // loop through second array and check if item in second array exists on created object.
+};
+
+// O(a+b)
