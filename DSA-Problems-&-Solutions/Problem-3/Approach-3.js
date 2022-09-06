455ee95a9efb28bd0612c9e1461f0572245bde9b
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-06 18:09:39 +0530

    06th Sept 2022 - C2

--- DSA-Problems-&-Solutions/Problem-3/Approach-3.js
+++ DSA-Problems-&-Solutions/Problem-3/Approach-3.js
@@ -1,15 +1,37 @@
+// Create a function that reverses a string/number(+/-)
+
+// For example:
+
+// 'Hi My name is Andrei' => 'ierdnA si eman yM iH'
+
+// 59350200 => 205395
+
+// -1589 => -9851
+
+// 543.2100 => 12.345
+
+// --------------------------------------------------
+
+/*
+
+[ Approach - 3 ]
+BIG-O Time Complexity = O()
+Space Complexity = O()
+
+Pros - Works for numbers (+/-) & strings
+
+*/
+
 const reverserFunc = (input) => {
-  if (!input || input.length < 2) {
-    return "That's not a good input!";
-  } else {
-    return [...input].reverse().join("");
-  }
+  return typeof input === "number"
+    ? parseFloat([...input.toString()].reverse().join("")) * Math.sign(input)
+    : input.split("").reverse().join("");
 };
 
 console.log(reverserFunc("Hi My name is Andrei"));
 console.log(reverserFunc(59350200));
-// console.log(reverserFunc(-1589));
-// console.log(reverserFunc(1));
-// console.log(reverserFunc("a"));
-// console.log(reverserFunc(543.21));
-// console.log(reverserFunc(-543.21));
+console.log(reverserFunc(-1589));
+console.log(reverserFunc(1));
+console.log(reverserFunc("a"));
+console.log(reverserFunc(543.21));
+console.log(reverserFunc(-5430.21));
